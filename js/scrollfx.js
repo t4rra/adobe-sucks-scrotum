e1169c198a44deb6f50beb560b74836086126b3a
E-Soda <64815861+E-Soda@users.noreply.github.com>
2021-09-12 22:01:38 -0700

    cssv2

---
+++ js/scrollfx.js
@@ -0,0 +1,30 @@
+const dark = document.querySelector("body");
+const light = document.querySelectorAll("h1, h2:not(#navtitle), p, .links a, .material-icons");
+
+
+window.onscroll = function () {
+  stickynav();
+  var top = window.scrollY;
+  if (top > window.innerHeight / 7) {
+    dark.classList.add("bg-dark");
+    for (let i = 0; i < light.length; i++) {
+      light[i].classList.add("fg-light");
+    }
+  } else {
+    dark.classList.remove("bg-dark");
+    for (let i = 0; i < light.length; i++) {
+      light[i].classList.remove("fg-light");
+    }
+  }
+};
+
+var navbar = document.querySelector("nav");
+var sticky = navbar.offsetTop;
+
+function stickynav() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
