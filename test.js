3eabe5fb819ad2fb21184a38f13aeb8e36d3480e
E-Soda <64815861+eaaasun@users.noreply.github.com>
2022-02-27 14:16:14 -0800

    finished js on test.html/test.js

--- test.js
+++ test.js
@@ -1,66 +1,47 @@
 // thanks https://photics.com/hero-1-help-my-map-does-not-work/
 
 function showElements(elements) {
-	for (var s = 0; s < elements.length; s++) {
-		elements[s].setAttribute("data-visible", "yep");
-	}
+  for (var s = 0; s < elements.length; s++) {
+    elements[s].setAttribute("data-visible", "yep");
+  }
 }
 function hideElements(elements) {
-	for (var h = 0; h < elements.length; h++) {
-		elements[h].setAttribute("data-visible", "nope");
-	}
+  for (var h = 0; h < elements.length; h++) {
+    elements[h].setAttribute("data-visible", "nope");
+  }
 }
 
 // function to log all elements
 function logAll(beans) {
-	for (var i = 0; i < beans.length; i++) {
-		beans[i].setAttribute("data-visible", "yep");
-		console.log(beans[i]);
-	}
+  for (var i = 0; i < beans.length; i++) {
+    console.log(beans[i]);
+  }
+}
+
+function selectAll(elements) {
+  for (var z = 0; z < elements.length; z++) {
+    return elements[z];
+  }
 }
 
 function update() {
-	var selectedMap = document.querySelector("input[name='mainFilter']:checked").value;
-	var secondSelectedMap = document.querySelectorAll("input[name='secondaryFilter']:checked").value;
-	console.log(selectedMap);
-	// console.log(secondSelectedMap);
-	logAll(document.querySelectorAll("input[name='secondaryFilter']:checked").value);
+  var selectedMap = document.querySelector("input[name='mainFilter']:checked").id;
+  var selectedMapClass = document.querySelector("input[name='mainFilter']:checked").value;
 
-	hideElements(document.getElementsByClassName("Physical"));
-	hideElements(document.getElementsByClassName("Political"));
-	hideElements(document.getElementsByClassName("Oceans"));
-	hideElements(document.getElementsByClassName("Flags"));
+  var selectedToggle = document.querySelector("input[name='secondaryfilter']:checked").id;
+  var selectedToggleClass = document.querySelector("input[name='secondaryfilter']:checked").value;
 
-	if (document.getElementById("button-all").checked) {
-		showElements(document.getElementsByClassName("toggleEL"));
-	} else {
-		hideElements(document.getElementsByClassName("toggleEL"));
+  console.log(selectedToggle);
+  console.log(selectedToggleClass);
+  hideElements(document.getElementsByClassName("toggleEl"));
+
+  if (document.getElementById(selectedMap).checked) {
+	  if (document.getElementById(selectedToggle).checked) {
+		showElements(document.querySelectorAll("." + selectedMapClass + "." + selectedToggleClass));
 	}
-
-	if (document.getElementById("button-physical").checked) {
-		showElements(document.getElementsByClassName("Physical"));
-	} else {
-		hideElements(document.getElementsByClassName("Physical"));
-	}
-
-	if (document.getElementById("button-political").checked) {
-		showElements(document.getElementsByClassName("Political"));
-	} else {
-		hideElements(document.getElementsByClassName("Political"));
-	}
-
-	if (document.getElementById("button-oceans").checked) {
-		showElements(document.getElementsByClassName("Oceans"));
-	} else {
-		hideElements(document.getElementsByClassName("Oceans"));
-	}
-
-	if (document.getElementById("button-flags").checked) {
-		showElements(document.getElementsByClassName("Flags"));
-	} else {
-		hideElements(document.getElementsByClassName("Flags"));
-	}
-
+  } else {
+    hideElements(document.getElementsByClassName(selectedMapClass));
+  }
 }
 
 document.addEventListener("input", update);
